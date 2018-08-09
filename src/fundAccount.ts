60057615a2758d27cbf9e36fc57f7ffe14a2dfa6
Carl Foster <carl.foster@abx.com>
2018-08-09 17:32:06 +1000

    Add funding logic

---
+++ src/fundAccount.ts
@@ -0,0 +1,40 @@
+import {
+  Server,
+  Operation,
+  Asset,
+  TransactionBuilder,
+  Keypair
+} from 'js-kinesis-sdk'
+
+const FUNDING_ACCOUNT_KEYPAIR = Keypair.fromSecret(
+  'SAR4YMRBHLU7P7UNPEAOQGSS5P3G4PNALDE3CNOCFNLJC2PA6TS3K44H'
+)
+const FUNDABLE_AMOUNT = 30 // lumens
+const AMOUNT_TO_STROOPS_FEE = 10e5 * 45
+
+export async function fundAccount(destination: string) {
+  const server = new Server('https://kau-testnet.kinesisgroup.io')
+  const accountExists = await server
+    .loadAccount(destination)
+    .then(_exists => true, _error => false)
+  const operation = accountExists
+    ? Operation.payment({
+        destination,
+        asset: Asset.native(),
+        amount: String(FUNDABLE_AMOUNT)
+      })
+    : Operation.createAccount({
+        destination: destination,
+        startingBalance: String(FUNDABLE_AMOUNT)
+      })
+  const fundingAccount = await server.loadAccount(
+    FUNDING_ACCOUNT_KEYPAIR.publicKey()
+  )
+  const transaction = new TransactionBuilder(fundingAccount, {
+    fee: String(FUNDABLE_AMOUNT * AMOUNT_TO_STROOPS_FEE)
+  })
+    .addOperation(operation)
+    .build()
+  transaction.sign(FUNDING_ACCOUNT_KEYPAIR)
+  await server.submitTransaction(transaction)
+}
--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,36 @@
 import server from 'server'
-import { render } from 'server/reply'
-import { get } from 'server/router'
+import { status, render, redirect, Reply } from 'server/reply'
+import { get, post } from 'server/router'
+import { fundAccount } from './fundAccount'
+
+const rateLimiter: { [key: string]: Date } = {}
+const limit = 1000 * 60 * 60
 
 server({ port: 3000, public: 'dist', views: 'dist' }, [
-  get('/', _ctx => render('index.html'))
+  get('/', _ => render('index.html')),
+  get('/*', _ => redirect('/')),
+  post('/:address', ctx => handleFundRequest(ctx.params.id))
 ])
+
+async function handleFundRequest(address: string): Promise<Reply> {
+  const requestTime = new Date()
+  if (isOverRateLimit(address, requestTime)) {
+    return status(429).json({ limitEnd: rateLimiter[address] })
+  }
+
+  try {
+    await fundAccount(address)
+  } catch (e) {
+    return status(400)
+  }
+
+  rateLimiter[address] = requestTime
+  return status(200)
+}
+
+function isOverRateLimit(address: string, currentTime: Date): boolean {
+  return (
+    rateLimiter[address] &&
+    currentTime.valueOf() - rateLimiter[address].valueOf() > limit
+  )
+}
